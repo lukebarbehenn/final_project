2d3c0eab79bee511e4f1d9015c87ab2abdd100a2
lukebarbehenn <lbarbehe@terpmail.umd.edu>
2024-04-08 14:11:39 -0400

    Create fp_code.py
    Basic layout of code

---
+++ fp_code.py
@@ -0,0 +1,25 @@
+# INST326 Final Project 
+
+class weather():
+
+    def getweather():
+        """Scrapes the web for current weather info"""
+        pass
+
+    def anaylzeweather():
+        """Provides an analysis about the weather"""
+        pass
+    
+    def predictweather():
+        """Makes predictions about future weather events"""
+        pass
+    
+    def reccomend():
+        """Reccomends to the user of how they can prepare for weather events"""
+        pass
+
+
+
+def main():
+    """What the user will interact with"""
+    pass
