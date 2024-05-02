3a6020ddc560f62623b6bcb18e834fb5d3926148
lukebarbehenn <lbarbehe@terpmail.umd.edu>
2024-05-01 22:56:51 -0400

    Update fp_code.py

--- fp_code.py
+++ fp_code.py
@@ -1,14 +1,31 @@
 # INST326 Final Project 
 
+from bs4 import BeautifulSoup
+import requests
+
 class weather():
 
-    def getweather():
+    def getweather(city, state):
         """Scrapes the web for current weather info"""
-        pass
 
+        api_url = f'https://weather.com/weather/tenday/l/{city}+{state}'
+
+        request = requests.get(api_url)
+
+        soup = BeautifulSoup(request.text, 'html.parser')
+
+        weather = soup.find('div')
+
+        print(weather)
+    
+   
+
+    
     def anaylzeweather():
         """Provides an analysis about the weather"""
-        pass
+        
+        cur_weather = weather.getweather("Gaithersburg", "MD")
+        
     
     def predictweather():
         """Makes predictions about future weather events"""
@@ -17,9 +34,4 @@ class weather():
     def reccomend():
         """Reccomends to the user of how they can prepare for weather events"""
         pass
-
-
-
-def main():
-    """What the user will interact with"""
-    pass
+    
