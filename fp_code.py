a05e58415ed2bf85d0df955aa9702e54412e7abb
lukebarbehenn <lbarbehe@terpmail.umd.edu>
2024-05-03 23:52:28 -0400

    Update fp_code.py
    Major changes

--- fp_code.py
+++ fp_code.py
@@ -1,74 +1,89 @@
 # INST326 Final Project 
 
-from bs4 import BeautifulSoup
 import requests
-
-def main():
-    # Prompt the user to enter a state
-    state = input("Enter a state (DC, VA, or MD): ").upper()
-    
-    # Define cities for each state
-    cities = {
-        'DC': ['Washington'],
-        'VA': ['Arlington', 'Fairfax', 'Alexandria'],
-        'MD': ['Rockville', 'Bethesda', 'College Park']
-    }
-    
-    # Check if the entered state is valid
-    if state in cities:
-        # Print the cities from the selected state
-        print(f"Select a city from {state}:")
-        for city in cities[state]:
-            print(city)
-    else:
-        print("Invalid state entered. Please enter DC, VA, or MD.")
-
-if __name__ == "__main__":
-    main()
-
 
 class weather():
 
-    def getweather():
+    def getweather(lat, lon):
         """Scrapes the web for current weather info"""
 
-        api_url = f'https://api.openweathermap.org/data/2.5/weather?lat=38.91982068626991&lon=-77.03656476393802&appid=8c6d96932235d74117595f9e8423547b'
+        api_url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=8c6d96932235d74117595f9e8423547b'
 
         request = requests.get(api_url)
 
-        json_convert = request.json()
+        x = request.json()
 
-        for x in json_convert:
+        temperature = x['main']['temp']
+        fahrenheit = (temperature - 273.15) * 1.8 + 32
+        
+        feelslike = x['main']['feels_like']
+        fahrenheit_fl = (feelslike - 273.15) * 1.8 + 32
 
-            temperature = x['temp'] 
-            fahrenheit = (temperature - 273.15) * 1.8 + 32
-            temp = x['main'].feelslike
-            humidity = x['humidity']
-            pressure = x['pressure']
-            visibility = x['visibility']
-            main_weather = x['main']
+        humidity = x['main']['humidity']
 
+        main_weather = x['weather'][0]['main']
 
-        return fahrenheit, humidity, pressure, visibility, main_weather
-            
+        ret_val = f'Weather: {main_weather} \nCurrent Temperature: {fahrenheit} \nFeels Like: {fahrenheit_fl} \nHumidity: {humidity}'
 
-   
-    # Dc Coords: 38.91982068626991, -77.03656476393802
-    # Arlington Va: 38.8869342864727, -77.12104283496652
-    # Alexandria Va: 38.81738004650533, -77.10816225921265
-    # Bethesda MD: 38.9755976253306, -77.11377134028886
-    # College Park MD: 39.00539708606798, -76.92506200115137
-
-   
+        return ret_val
+    
     def anaylzeweather(): 
         """Provides an analysis about the weather (DON)"""
         
         cur_weather = weather.getweather("Gaithersburg", "MD")
         
     
-    def predictweather():
-        """Makes predictions about future weather events"""
+    def reccomend_and_predict():
+        """Makes reccomendations or predictions to the user based on weather """
         pass
     
-    
+
+def main():
+
+    """This is what the user will interact with"""
+
+    dc_lat = 38.91982068626991
+    dc_lon = -77.03656476393802
+
+    arl_lat = 38.8869342864727
+    arl_lon = -77.12104283496652
+
+    alx_lat = 38.81738004650533
+    alx_lon = -77.10816225921265
+
+    bsd_lat = 38.9755976253306
+    bsd_lon = -77.11377134028886
+
+    cp_lat = 39.00539708606798
+    cp_lon = -76.92506200115137
+
+        
+    user_input = input("Enter a state. DC, MD, or VA: ").upper()
+
+    if user_input == "DC":
+        result = weather.getweather(dc_lat, dc_lon)
+        print(result)
+
+    if user_input == "VA":
+        va_state_input = input("Enter a City. Arlington or Alexandria: ").lower()
+        if va_state_input == "arlington":
+            result = weather.getweather(arl_lat, arl_lon)
+            print(result)
+        if va_state_input == "alexandria":
+            result = weather.getweather(alx_lat, alx_lon)
+            print(result)
+
+    if user_input == "MD": 
+        md_state_input = input("Enter a City. Bethesda or College Park: ").lower()
+        if md_state_input == "bethesda":
+            result = weather.getweather(bsd_lat, bsd_lon)
+            print(result)
+        if md_state_input == "college park":
+            result = weather.getweather(cp_lat, cp_lon)
+            print(result)
+
+         
+if __name__ == "__main__":
+    main()
+
     
